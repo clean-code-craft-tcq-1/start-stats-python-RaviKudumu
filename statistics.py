d6326c4221166c715feb972426ef45d627cd1411
Ravi Kudumu <ravikudumu@gmail.com>
2021-01-22 12:53:23 +0530

    Update statistics.py

--- statistics.py
+++ statistics.py
@@ -1,3 +1,11 @@
-
+import numpy as np   
 def calculateStats(numbers):
   return None
+ 
+def report_min_max_avg(a):
+    a = np.array(a)
+    dict_stat = {}
+    dict_stat["min"] = a.min()
+    dict_stat["max"] = a.max()
+    dict_stat["avg"] = a.mean()
+    return dict_stat
